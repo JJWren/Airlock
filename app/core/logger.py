a261cb1d40d71fcd61175eb0ebc273ebfb11ca30
Joshua Mykitta <joshua.j.wren@gmail.com>
2026-03-24 01:40:58 -0500

    feat: updated logger to pull log level from docker-compose.yml

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,9 +1,11 @@
-import sys
+import os, sys
 from loguru import logger
 
 
 def setup_logging():
     """Configures Loguru to handle all application logs."""
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+
     # Remove default handler
     logger.remove()
 
@@ -11,9 +13,11 @@ def setup_logging():
     logger.add(
         sys.stdout,
         format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{message}</cyan>",
-        level="DEBUG",
+        level=log_level,
         enqueue=True  # Keeps the logger from blocking the main scan logic
     )
+
+    logger.info(f"📜 Logger initialized at level: {log_level}")
     return logger
 
 
